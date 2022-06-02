dab3b282bc24a837b0a7ce31c2d899ac48fb3ea0
balaji sainath <balajisainath@balajis-MacBook-Air.local>
2022-06-02 23:53:52 +0530

    insertionsort

---
+++ arrays/insertionsort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n=0;
+    int arr[n];
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    for (int i=1;i<n;i++){
+        int current=arr[i];
+        int j=i-1;
+    for (;j>=0;j--){
+        if (arr[j]>current){
+            arr[j+1]=arr[j];
+        }
+        else{
+            break;
+        }
+    }
+    arr[j+1]=current;
+        
+        
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
